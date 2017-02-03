e984b2a9173efdc43e7bbe6de2f88fdfb9c9d226
Saugat Rai <Saugat Rai>
2017-02-03 08:09:05 +0545

    New Commit

--- app/src/main/java/com/example/rai/sharedpreferencesdemo/MainActivity.java
+++ app/src/main/java/com/example/rai/sharedpreferencesdemo/MainActivity.java
@@ -13,24 +13,43 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
     String MYPREFERENCES ="ASMT";
     String Name = "user_name";
-    EditText edit1;
+    String Phone = "user_phone";
+    String Email= "user_email";
+    EditText name, phone, email;
     Button bset, bget, bclear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edit1 = (EditText)findViewById(R.id.editText);
+        name = (EditText)findViewById(R.id.editName);
+        phone = (EditText)findViewById(R.id.editPhone);
+        email = (EditText)findViewById(R.id.editEmail);
         bset = (Button) findViewById(R.id.btnSet);
+
 
         bset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    String user = edit1.getText().toString();
-                sharedPreferences = getSharedPreferences(MYPREFERENCES, MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString(Name, user);
-                editor.commit();
-
+                String user = name.getText().toString();
+                String emailAdd = email.getText().toString();
+                String PhoneNo = phone.getText().toString();
+                int ph = Integer.parseInt(PhoneNo);
+                if(ph != 10)
+                {
+                    Toast.makeText(MainActivity.this, "Give 10 number for phone", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    sharedPreferences = getSharedPreferences(MYPREFERENCES, MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString(Name, user);
+                    editor.putString(Email, emailAdd);
+                    editor.putString(Phone, PhoneNo);
+                    Toast.makeText(MainActivity.this, "Data Entered Successfully", Toast.LENGTH_SHORT).show();
+                    name.setText("");
+                    email.setText("");
+                    phone.setText("");
+                    editor.commit();
+                }
             }
         });
         bget = (Button)findViewById(R.id.btnGet);
@@ -39,8 +58,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 sharedPreferences = getSharedPreferences(MYPREFERENCES, MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
-                String msg = sharedPreferences.getString(Name, "No name");
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                String na = sharedPreferences.getString(Name, "No name");
+                String em = sharedPreferences.getString(Email, "No name");
+                String ph = sharedPreferences.getString(Phone, "No name");
+                name.setText(na);
+                phone.setText(ph);
+                email.setText(em);
+                Toast.makeText(MainActivity.this, "Data Retrived Successfully", Toast.LENGTH_SHORT).show();
                 editor.commit();
             }
         });
@@ -52,6 +76,10 @@ public class MainActivity extends AppCompatActivity {
                 sharedPreferences = getSharedPreferences(MYPREFERENCES, MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.clear();
+                name.setText("");
+                email.setText("");
+                phone.setText("");
+                Toast.makeText(MainActivity.this, "Data Cleared", Toast.LENGTH_SHORT).show();
                 editor.commit();
             }
         });
